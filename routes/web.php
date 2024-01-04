8a9c871aa13898f07f5b0e72a10eea7420368c65
Ilir <ilir.perolli@tenton.co>
2024-01-04 01:10:46 +0100

    fix: tweaks

--- routes/web.php
+++ routes/web.php
@@ -36,15 +36,14 @@ Route::get('/', function () {
 
 //Route::get('/', JobsIndex::class)->name('jobs.index');
 
-Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::get('/dashboard', Dashboard::class)->name('dashboard');
+//Route::get('/dashboard', Dashboard::class)->middleware(['auth', 'verified'])->name('dashboard');
+//
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::get('/tenders', TendersIndex::class)->name('tenders.index');
-    Route::get('/jobs', JobsIndex::class)->name('jobs.index');
 
     Route::get('/providers', ProvidersIndex::class)->name('admin.providers.index');
     Route::get('/providers/create', ProvidersCreate::class)->name('admin.providers.create');
@@ -58,5 +57,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/cities/create', CitiesCreate::class)->name('admin.cities.create');
     Route::get('/cities/{slug}/edit', CitiesEdit::class)->name('admin.cities.edit');
 });
+
+Route::get('/tenders', TendersIndex::class)->name('tenders.index');
+Route::get('/jobs', JobsIndex::class)->name('jobs.index');
 
 require __DIR__.'/auth.php';

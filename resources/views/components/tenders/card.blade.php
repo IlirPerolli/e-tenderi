41860f798f2838ee6f7bd2d1835d91a72f890879
Ilir <ilir.perolli@tenton.co>
2024-01-05 16:34:06 +0100

    Update card.blade.php

--- resources/views/components/tenders/card.blade.php
+++ resources/views/components/tenders/card.blade.php
@@ -5,8 +5,8 @@
         <div class="p-4 flex">
             <div class="w-16 h-16 mr-4 flex-shrink-0">
                 @dump($item?->provider?->image_path)
-                <img onerror="this.src = '{{ default_404_image() }}'"
-                     src="{{$item->image_path ?? $item?->provider?->image_path}}" alt="Company Logo"
+                <img
+                     src="{{$item->image_path ?? $item?->provider?->image_path ?? default_404_image()}}" alt="Company Logo"
                      class="w-full h-full object-cover rounded-full">
             </div>
             <div class="flex-grow">
